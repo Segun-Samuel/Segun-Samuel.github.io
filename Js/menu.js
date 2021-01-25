8fc1b2279d3058d0eee01dd204953ad3f04bdc19
Segun-Samuel <samuelsegun39@gmail.com>
2021-01-26 00:28:06 +0100

    First commit

---
+++ Js/menu.js
@@ -0,0 +1,8 @@
+  function openApps(){
+    document.getElementById("Appsblock").style.visibility="visible";
+    document.getElementById("bars").style.visibility="visible";
+  }
+  function closeApps(){
+    document.getElementById("Appsblock").style.visibility="hidden";
+    document.getElementById("bars").style.visibility="hidden";
+  }
